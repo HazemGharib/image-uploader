a12632773aa6e8fedb0266cf0e7c4bafff3ad724
HazemGharib <hazem.gharib.8@gmail.com>
2017-05-06 01:58:18 +0200

    Adding portfolio feature

--- server.js
+++ server.js
@@ -52,6 +52,12 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects Page',
+    });
+});
+
 app.get('/bad', (req, res) => {
     res.send({
         errorMessage: "Unable to handle that request"
