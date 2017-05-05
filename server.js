e4b2a6c9e5145395d00fd978c59cb946e4223cfc
HazemGharib <hazem.gharib.8@gmail.com>
2017-05-05 20:11:11 +0200

    Initial Commit

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `\r\n${now}: [${req.method}] ${req.url}`;
+    console.log(log);
+    fs.appendFile(`server.log`, log, { encoding: `utf-8` });
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintainence.hbs',{});
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear().toString();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    //res.send('<h1>Hello Express!</h1>');
+    // res.send({
+    //     name: 'Hazem',
+    //     likes: [
+    //         'Football',
+    //         'Travelling'
+    //     ]
+    // });
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome in my brand new website'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: "Unable to handle that request"
+    });
+});
+
+app.listen(3000, () => {
+    console.log(`Server has started at port 3000 !`);
+});
